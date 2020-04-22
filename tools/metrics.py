466aff8af885b8aafa6045fb831519d01a6f2777
stijn <stijn@ignitron.net>
2020-04-22 09:24:28 +0200

    [wip] tools/metrics: Add windows port.

--- tools/metrics.py
+++ tools/metrics.py
@@ -62,6 +62,7 @@ port_data = {
     "m": PortData("minimal x86", "minimal", "build/firmware.elf"),
     "u": PortData("unix x64", "unix", "micropython"),
     "n": PortData("unix nanbox", "unix", "micropython-nanbox", "VARIANT=nanbox"),
+    "w": PortData("windows x64", "windows", "micropython.exe", "CROSS_COMPILE=i686-w64-mingw32-"),
     "s": PortData("stm32", "stm32", "build-PYBV10/firmware.elf", "BOARD=PYBV10"),
     "c": PortData("cc3200", "cc3200", "build/WIPY/release/application.axf", "BTARGET=application"),
     "8": PortData("esp8266", "esp8266", "build-GENERIC/firmware.elf"),
